3cc517ddd1983394ff88d4f77ea8cbca9f61b4d8
leminh4000 <leminh2000@gmail.com>
2022-08-23 15:19:56 +0000

    3. argument parsing

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-const chalk = require('chalk')
-const getNotes = require('./notes.js')
+// const chalk = require('chalk')
+// const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
-const command = process.argv[2]
-
-console.log(process.argv)
-
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+console.log(yargs.argv)
